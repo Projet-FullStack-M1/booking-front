984b16f0d8d02ad15bb9bc0fb6c754cff47ad912
MalicknND <ndiayemalick062@gmail.com>
2024-04-21 01:00:27 +0200

    💄 Responsive Layout with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
+import "./layout.scss";
 const App = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <div className="layout">Hello</div>;
 };
 
 export default App;
