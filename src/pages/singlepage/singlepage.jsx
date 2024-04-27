bdcc5ba153f76149c8b96c4f3ec0e69ef8557ed3
MalicknND <ndiayemalick062@gmail.com>
2024-04-27 22:01:53 +0200

    save work

--- src/pages/singlepage/singlepage.jsx
+++ src/pages/singlepage/singlepage.jsx
@@ -2,8 +2,12 @@ import styles from "./singlepage.module.scss";
 const Singlepage = () => {
   return (
     <div className={styles.singlepage}>
-      <div className={styles.details}>details</div>
-      <div className={styles.features}>features</div>
+      <div className={styles.details}>
+        <div className={styles.wrapper}></div>
+      </div>
+      <div className={styles.features}>
+        <div className={styles.wrapper}></div>
+      </div>
     </div>
   );
 };
